c6c32f40081aa5402e58e5c238e1497fa92320b5
jrarick <36737588+jrarick@users.noreply.github.com>
2020-01-28 08:53:13 -0500

    Add files via upload

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,14 +1,22 @@
 import Link from "next/link"
+import gsap from "gsap"
+import { useEffect } from "react"
 
 export default ({ isOpen }) => {
-  if (!isOpen) {
-    return null
-  }
+  useEffect(() => {
+    gsap.to('#nav', { duration: 0.5, ease: "elastic.out(1, 0.8)", scaleX: isOpen ? 1 : 0 })
+  }, [isOpen])
+
   return (
-    <nav>
+    <nav id="nav">
       <Link href="/checkout">
         <a>Check Out</a>
       </Link>
+      <style jsx>{`
+        #nav {
+          margin: 10px;
+        }
+      `}</style>
     </nav>
   )
 }
